fee7310e6c67607d715c0067c1254cc05f063550
m-salmanqureshi <thedesignerofficial92@gmail.com>
2025-05-03 06:00:10 -0700

    Admin login function by Salman

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,3 +29,73 @@ void createQuiz();
 void takeQuiz();
 void viewResults();
 void timerDelay(int seconds);
+
+//Student registration function
+void registerStudent() {
+    FILE *fp;
+    Student s;
+
+    fp = fopen("students.txt", "a");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    printf("Enter Name: ");
+    scanf(" %[^\n]", s.name);
+    printf("Enter ID: ");
+    scanf("%s", s.id);
+    printf("Enter Password: ");
+    scanf("%s", s.password);
+
+    fwrite(&s, sizeof(Student), 1, fp);
+    fclose(fp);
+
+    printf("Registration Successful!\n");
+}
+
+int loginStudent() {
+    FILE *fp;
+    Student s;
+    char id[20], password[20];
+    int found = 0;
+
+    fp = fopen("students.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 0;
+    }
+
+    printf("Enter ID: ");
+    scanf("%s", id);
+    printf("Enter Password: ");
+    scanf("%s", password);
+
+    while (fread(&s, sizeof(Student), 1, fp)) {
+        if (strcmp(s.id, id) == 0 && strcmp(s.password, password) == 0) {
+            printf("Login Successful!\n");
+            found = 1;
+            break;
+        }
+    }
+    fclose(fp);
+
+    if (!found) {
+        printf("Login Failed. Try again.\n");
+    }
+    return found;
+}
+
+int adminLogin() {
+    char adminPass[20];
+    printf("Enter Admin Password: ");
+    scanf("%s", adminPass);
+
+    if (strcmp(adminPass, "admin123") == 0) {
+        printf("Admin Login Successful!\n");
+        return 1; // success
+    } else {
+        printf("Invalid Admin Password!\n");
+        return 0; // failed
+    }
+}
